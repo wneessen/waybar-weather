34ab1152c51c5fa06c537b1b9a960fa88f10229a
Winni Neessen <wn@neessen.dev>
2025-11-17 10:29:54 +0000

    Refactored i18n initialization and removed environment-based locale detection
    - Removed `languageFromEnv` function and environment variable-based locale detection logic.
    - Added fallback language (`English`) for improved resilience in i18n initialization.
    - Simplified supported language handling by removing hard-coded `supportedLanguages`.
    
    Signed-off-by: Winni Neessen <wn@neessen.dev>

--- internal/i18n/i18n.go
+++ internal/i18n/i18n.go
@@ -4,17 +4,11 @@ import (
 	"embed"
 	"fmt"
 	"io/fs"
-	"os"
 
 	"github.com/Xuanwo/go-locale"
 	"github.com/vorlif/spreak"
 	"golang.org/x/text/language"
 )
-
-var supportedLanguages = []language.Tag{
-	language.English,
-	language.German,
-}
 
 //go:embed locale/*
 var locales embed.FS
@@ -36,6 +30,7 @@ func New(loc string) (*spreak.Localizer, error) {
 
 	bundle, err := spreak.NewBundle(
 		spreak.WithSourceLanguage(language.English),
+		spreak.WithFallbackLanguage(language.English),
 		spreak.WithDomainFs("", localeFS),
 		spreak.WithLanguage(tag),
 	)
@@ -43,21 +38,4 @@ func New(loc string) (*spreak.Localizer, error) {
 		return nil, fmt.Errorf("failed to create i18n bundle: %w", err)
 	}
 	return spreak.NewLocalizer(bundle, tag), nil
-}
-
-// languageFromEnv returns the language tag for the current locale.
-//
-// According to https://www.ibm.com/docs/en/aix/7.1.0?topic=locales-understanding-locale-environment-variables
-// LC_ALL, LC_MESSAGES and LANG are highest to lowest priority.
-func languageFromEnv() language.Tag {
-	lang := os.Getenv("LANG")
-	if val := os.Getenv("LC_MESSAGES"); val != "" {
-		lang = val
-	}
-	if val := os.Getenv("LC_ALL"); val != "" {
-		lang = val
-	}
-	matcher := language.NewMatcher(supportedLanguages)
-	tag, _, _ := matcher.Match(language.Make(lang))
-	return tag
 }
