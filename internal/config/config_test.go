947aafc40554a940dd79c5c33d8616078081c1b7
Winni Neessen <wn@neessen.dev>
2025-11-20 22:31:34 +0000

    Improved configuration testing and handling for invalid files
    - Removed locale-specific tests for simplifying configuration validation.
    - Added `TestNewFromFile` test cases to validate configuration loading from file.
    - Included tests for handling invalid and non-existent configuration files.
    - Added a new invalid configuration file sample for testing.
    - 100% test coverage for internal/config
    
    Signed-off-by: Winni Neessen <wn@neessen.dev>

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -13,7 +13,6 @@ func TestNew(t *testing.T) {
 		expectWeatherForecastHours  = 3
 		expectIntervalWeatherUpdate = time.Minute * 15
 		expectIntervalOutput        = time.Second * 30
-		expectLocale                = "en-US"
 	)
 	t.Run("new config with all defaults set", func(t *testing.T) {
 		conf, err := New()
@@ -64,22 +63,50 @@ func TestNew(t *testing.T) {
 			t.Error("expected config to fail, but didn't")
 		}
 	})
-	t.Run("correct locale should be set", func(t *testing.T) {
-		t.Setenv("LC_MESSAGES", "en_US.UTF-8")
-		conf, err := New()
+}
+
+func TestNewFromFile(t *testing.T) {
+	const (
+		expectDefaultUnits          = "imperial"
+		expectLogLevel              = slog.LevelInfo
+		expectWeatherForecastHours  = 5
+		expectIntervalWeatherUpdate = time.Minute * 30
+		expectIntervalOutput        = time.Second * 15
+	)
+	t.Run("reading config from valid file succeeds", func(t *testing.T) {
+		conf, err := NewFromFile("etc", "waybar-weather.toml")
 		if err != nil {
-			t.Errorf("failed to load config: %s", err)
+			t.Fatalf("failed to load config: %s", err)
 		}
-		if conf.Locale != expectLocale {
-			t.Errorf("expected locale to be: %s, got %s", expectLocale, conf.Locale)
+		if conf.Units != expectDefaultUnits {
+			t.Errorf("expected units to be: %s, got %s", expectDefaultUnits, conf.Units)
+		}
+		if conf.LogLevel != expectLogLevel {
+			t.Errorf("expected log level to be: %s, got %s", expectLogLevel, conf.LogLevel)
+		}
+		if conf.Weather.ForecastHours != expectWeatherForecastHours {
+			t.Errorf("expected weather forecast hours to be: %d, got %d", expectWeatherForecastHours,
+				conf.Weather.ForecastHours)
+		}
+		if conf.Intervals.WeatherUpdate != expectIntervalWeatherUpdate {
+			t.Errorf("expected weather update interval to be: %s, got %s", expectIntervalWeatherUpdate,
+				conf.Intervals.WeatherUpdate)
+		}
+		if conf.Intervals.Output != expectIntervalOutput {
+			t.Errorf("expected output interval to be: %s, got %s", expectIntervalOutput, conf.Intervals.Output)
 		}
 	})
-	t.Run("invalid locale should be ignored", func(t *testing.T) {
-		t.Setenv("LC_MESSAGES", "invalid")
-		conf, err := New()
-		if err != nil {
-			t.Errorf("failed to load config: %s", err)
+	t.Run("reading config from non-existent file fails", func(t *testing.T) {
+		_, err := NewFromFile("etc", "non-existent.toml")
+		if err == nil {
+			t.Error("expected config to fail, but didn't")
 		}
-		t.Logf("locale: %s", conf.Locale)
+	})
+	t.Run("reading invalid config file fails", func(t *testing.T) {
+		_, err := NewFromFile("testdata", "invalid.toml")
+		t.Logf("error: %s", err)
+		if err == nil {
+			t.Error("expected config to fail, but didn't")
+		}
 	})
 }
