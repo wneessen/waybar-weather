52e27cbe17877ff3f6e440d424eac4601977511a
Winni Neessen <wn@neessen.dev>
2025-11-23 23:09:09 +0000

    Added unit tests for ICHNAEA geolocation provider
    - Implemented comprehensive test cases for `NewGeolocationICHNAEAProvider` creation, both success and failure scenarios.
    - Added tests for `locate`, covering accurate geolocation results and invalid JSON handling.
    - Tested provider's `Name` method verification and `createResult` functionality.
    - Introduced tests for Wi-Fi scanning scenarios, including monitoring Wi-Fi access points.
    - Ensured skipped tests for systems without Wi-Fi support or no available access points.
    
    Signed-off-by: Winni Neessen <wn@neessen.dev>

---
+++ internal/geobus/provider/ichnaea/ichnaea_test.go
@@ -0,0 +1,212 @@
+// SPDX-FileCopyrightText: Winni Neessen <wn@neessen.dev>
+//
+// SPDX-License-Identifier: MIT
+
+package ichnaea
+
+import (
+	"context"
+	"io"
+	"log/slog"
+	stdhttp "net/http"
+	"os"
+	"strings"
+	"testing"
+	"testing/synctest"
+
+	"github.com/mdlayher/wifi"
+
+	"github.com/wneessen/waybar-weather/internal/geobus"
+	"github.com/wneessen/waybar-weather/internal/http"
+	"github.com/wneessen/waybar-weather/internal/logger"
+	"github.com/wneessen/waybar-weather/internal/testhelper"
+)
+
+const (
+	testFile = "../../../../testdata/beacondb.json"
+	testLat  = 40.7185
+	testLon  = -74.0025
+	testAcc  = 2000
+)
+
+func TestNewGeolocationICHNAEAProvider(t *testing.T) {
+	testRequiresWiFi(t)
+	t.Run("new ICHNAEA provider succeeds", func(t *testing.T) {
+		provider, err := NewGeolocationICHNAEAProvider(http.New(logger.New(slog.LevelInfo)))
+		if err != nil {
+			t.Fatalf("failed to create ICHNAEA provider: %s", err)
+		}
+		if provider == nil {
+			t.Fatal("expected provider to be non-nil")
+		}
+	})
+	t.Run("ICHNAEA without http client fails ", func(t *testing.T) {
+		provider, err := NewGeolocationICHNAEAProvider(nil)
+		if err == nil {
+			t.Fatal("expected provider to fail")
+		}
+		if provider != nil {
+			t.Fatal("expected provider to be nil")
+		}
+	})
+}
+
+func TestGeolocationICHNAEAProvider_Name(t *testing.T) {
+	provider, err := NewGeolocationICHNAEAProvider(http.New(logger.New(slog.LevelInfo)))
+	if err != nil {
+		t.Fatalf("failed to create ICHNAEA provider: %s", err)
+	}
+	if !strings.EqualFold(provider.Name(), name) {
+		t.Errorf("expected provider name to be %s, got %s", name, provider.Name())
+	}
+}
+
+// This test is very flacky, since it depends on the WiFi hardware
+func TestNewGeolocationICHNAEAProvider_wifiList(t *testing.T) {
+	testRequiresWiFi(t)
+
+	provider, err := NewGeolocationICHNAEAProvider(http.New(logger.New(slog.LevelInfo)))
+	if err != nil {
+		t.Fatalf("failed to create ICHNAEA provider: %s", err)
+	}
+	list, err := provider.wifiAccessPoints()
+	if err != nil {
+		t.Fatalf("failed to get WiFi list: %s", err)
+	}
+	if len(list) == 0 {
+		t.Skip("no WiFi access points found, test results are meaningless")
+	}
+}
+
+func TestGeolocationICHNAEAProvider_locate(t *testing.T) {
+	t.Run("locate succeeds with different accuracies", func(t *testing.T) {
+		rtFn := func(req *stdhttp.Request) (*stdhttp.Response, error) {
+			data, err := os.Open(testFile)
+			if err != nil {
+				t.Fatalf("failed to open JSON response file: %s", err)
+			}
+
+			return &stdhttp.Response{
+				StatusCode: 200,
+				Body:       data,
+				Header:     make(stdhttp.Header),
+			}, nil
+		}
+		client := http.New(logger.New(slog.LevelInfo))
+		client.Transport = testhelper.MockRoundTripper{Fn: rtFn}
+		provider, err := NewGeolocationICHNAEAProvider(client)
+		if err != nil {
+			t.Fatalf("failed to create ICHNAEA provider: %s", err)
+		}
+
+		lat, lon, acc, err := provider.locate(t.Context())
+		if err != nil {
+			t.Fatalf("failed to locate coordinates via ICHNAEA: %s", err)
+		}
+		if lat != testLat {
+			t.Errorf("expected latitude to be %f, got %f", testLat, lat)
+		}
+		if lon != testLon {
+			t.Errorf("expected longitude to be %f, got %f", testLon, lon)
+		}
+		if geobus.Truncate(acc, 1) != geobus.Truncate(testAcc, 1) {
+			t.Errorf("expected accuracy to be %f, got %f", geobus.Truncate(testAcc, 1),
+				geobus.Truncate(acc, 1))
+		}
+	})
+	t.Run("locate fails with broken JSON", func(t *testing.T) {
+		rtFn := func(req *stdhttp.Request) (*stdhttp.Response, error) {
+			return &stdhttp.Response{
+				StatusCode: 200,
+				Body:       io.NopCloser(strings.NewReader("NOT_JSON")),
+				Header:     make(stdhttp.Header),
+			}, nil
+		}
+		client := http.New(logger.New(slog.LevelInfo))
+		client.Transport = testhelper.MockRoundTripper{Fn: rtFn}
+		provider, err := NewGeolocationICHNAEAProvider(client)
+		if err != nil {
+			t.Fatalf("failed to create ICHNAEA provider: %s", err)
+		}
+
+		_, _, _, err = provider.locate(t.Context())
+		if err == nil {
+			t.Fatal("expected locate to fail")
+		}
+	})
+}
+
+func TestGeolocationICHNAEAProvider_createResult(t *testing.T) {
+	provider, err := NewGeolocationICHNAEAProvider(http.New(logger.New(slog.LevelInfo)))
+	if err != nil {
+		t.Fatalf("failed to create GeoIP provider: %s", err)
+	}
+	result := provider.createResult("test", geobus.Coordinate{Lat: testLat, Lon: testLon, Acc: geobus.AccuracyCity})
+	if result.Lat != testLat {
+		t.Errorf("expected latitude to be %f, got %f", testLat, result.Lat)
+	}
+	if result.Lon != testLon {
+		t.Errorf("expected longitude to be %f, got %f", testLon, result.Lon)
+	}
+	if result.Key != "test" {
+		t.Errorf("expected key to be %s, got %s", "test", result.Key)
+	}
+	if result.AccuracyMeters != geobus.AccuracyCity {
+		t.Errorf("expected accuracy to be %d, got %f", geobus.AccuracyCity, result.AccuracyMeters)
+	}
+	if result.Source != provider.Name() {
+		t.Errorf("expected source to be %s, got %s", provider.Name(), result.Source)
+	}
+	if result.TTL != provider.ttl {
+		t.Errorf("expected TTL to be %d, got %d", provider.ttl, result.TTL)
+	}
+}
+
+func TestNewGeolocationICHNAEAProvider_monitorWifiAccessPoints(t *testing.T) {
+	testRequiresWiFi(t)
+	t.Run("monitor WiFi access points succeeds", func(t *testing.T) {
+		synctest.Test(t, func(t *testing.T) {
+			ctx, cancel := context.WithCancel(t.Context())
+			defer cancel()
+
+			isCancelled := false
+			context.AfterFunc(ctx, func() {
+				isCancelled = true
+			})
+
+			provider, err := NewGeolocationICHNAEAProvider(http.New(logger.New(slog.LevelInfo)))
+			if err != nil {
+				t.Fatalf("failed to create ICHNAEA provider: %s", err)
+			}
+			go provider.monitorWifiAccessPoints(ctx)
+			synctest.Wait()
+			cancel()
+			synctest.Wait()
+			if !isCancelled {
+				t.Fatal("expected monitor to be cancelled")
+			}
+		})
+	})
+}
+
+func testRequiresWiFi(t *testing.T) {
+	wlan, err := wifi.New()
+	if err != nil {
+		t.Skip("system has no WiFi support, skipping WiFi related tests")
+	}
+
+	checkIfaces := make([]*wifi.Interface, 0)
+	ifaces, err := wlan.Interfaces()
+	if err != nil {
+		t.Skip("no WiFi interfaces found, skipping WiFi related tests")
+	}
+	for _, iface := range ifaces {
+		if iface.Type != wifi.InterfaceTypeStation {
+			continue
+		}
+		checkIfaces = append(checkIfaces, iface)
+	}
+	if len(checkIfaces) == 0 {
+		t.Skip("no WiFi interfaces found, skipping WiFi related tests")
+	}
+}
