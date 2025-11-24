f80a9dc26097574126822b6110f9d4d4556423fa
Winni Neessen <wn@neessen.dev>
2025-11-24 13:45:45 +0000

    Removed redundant package comment in `gpspoll`
    - Deleted unnecessary package-level documentation in `gpspoll.go`, as it duplicated existing information.
    
    Signed-off-by: Winni Neessen <wn@neessen.dev>

--- internal/gpspoll/gpspoll.go
+++ internal/gpspoll/gpspoll.go
@@ -2,8 +2,6 @@
 //
 // SPDX-License-Identifier: MIT
 
-// Package gpspoll provides a minimal, goroutine-safe GPSd client that
-// performs a single POLL request to retrieve the current fix.
 package gpspoll
 
 import (
