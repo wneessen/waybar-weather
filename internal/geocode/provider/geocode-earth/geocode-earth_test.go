2fed77a62e2c8ccb918ffa4abeb73e4961ac491e
Winni Neessen <wn@neessen.dev>
2025-11-27 19:09:18 +0000

    Added unit tests for `geocode-earth` provider implementation
    - Introduced test suite for `geocode-earth` provider, covering initialization and behavior.
    - Verified `Reverse` functionality for different locations, including city, town, and village scenarios.
    - Added test cases for caching logic and API error handling.
    - Included integration tests to validate behavior with live Geocode Earth API.
    - Added test JSON files for mocked API responses.
    
    Signed-off-by: Winni Neessen <wn@neessen.dev>

---
+++ internal/geocode/provider/geocode-earth/geocode-earth_test.go
@@ -0,0 +1,274 @@
+// SPDX-FileCopyrightText: Winni Neessen <wn@neessen.dev>
+//
+// SPDX-License-Identifier: MIT
+
+package geocode_earth
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+	"log/slog"
+	stdhttp "net/http"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"golang.org/x/text/language"
+
+	"github.com/wneessen/waybar-weather/internal/geocode"
+	"github.com/wneessen/waybar-weather/internal/http"
+	"github.com/wneessen/waybar-weather/internal/logger"
+	"github.com/wneessen/waybar-weather/internal/testhelper"
+)
+
+const (
+	cityExpected = "A.T. Kearney, Berlin, Germany"
+	cityFile     = "../../../../testdata/geocodeearth_berlin.json"
+	cityLat      = 52.5129
+	cityLon      = 13.3910
+	testHitTTL   = 1 * time.Second
+	testMissTTL  = 1 * time.Second
+
+	villageExpected = "Marshfield"
+	villageFile     = "../../../../testdata/geocodeearth_marshfield.json"
+	villageLat      = 51.46292
+	villageLon      = -2.31850
+
+	townExpected = "Otley"
+	townFile     = "../../../../testdata/geocodeearth_otley.json"
+	townLat      = 53.90712
+	townLon      = -1.69404
+)
+
+func TestNew(t *testing.T) {
+	t.Run("creating a new provider succeeds", func(t *testing.T) {
+		coder := testCoder(t)
+		if coder == nil {
+			t.Fatal("expected a non-nil geocoder")
+		}
+	})
+	t.Run("provider name is correct", func(t *testing.T) {
+		coder := testCoder(t)
+		if coder.Name() != name {
+			t.Errorf("expected provider name to be %q, got %q", name, coder.Name())
+		}
+	})
+}
+
+func TestOpenCage_Reverse(t *testing.T) {
+	t.Run("reverse geocoding succeeds", func(t *testing.T) {
+		rtFn := func(req *stdhttp.Request) (*stdhttp.Response, error) {
+			data, err := os.Open(cityFile)
+			if err != nil {
+				t.Fatalf("failed to open JSON response file: %s", err)
+			}
+
+			return &stdhttp.Response{
+				StatusCode: 200,
+				Body:       data,
+				Header:     make(stdhttp.Header),
+			}, nil
+		}
+
+		coder := testCoderWithRoundtripFunc(t, rtFn)
+		addr, err := coder.Reverse(t.Context(), cityLat, cityLon)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !addr.AddressFound {
+			t.Fatal("expected address to be found")
+		}
+		if !strings.EqualFold(addr.DisplayName, cityExpected) {
+			t.Errorf("expected address to be %q, got %q", cityExpected, addr.DisplayName)
+		}
+	})
+	t.Run("reverse cached geocoding succeeds", func(t *testing.T) {
+		rtFn := func(req *stdhttp.Request) (*stdhttp.Response, error) {
+			data, err := os.Open(cityFile)
+			if err != nil {
+				t.Fatalf("failed to open JSON response file: %s", err)
+			}
+
+			return &stdhttp.Response{
+				StatusCode: 200,
+				Body:       data,
+				Header:     make(stdhttp.Header),
+			}, nil
+		}
+
+		coder := geocode.NewCachedGeocoder(testCoderWithRoundtripFunc(t, rtFn), testHitTTL, testMissTTL)
+		addr, err := coder.Reverse(t.Context(), cityLat, cityLon)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !addr.AddressFound {
+			t.Fatal("expected address to be found")
+		}
+		if !strings.EqualFold(addr.DisplayName, cityExpected) {
+			t.Errorf("expected address to be %q, got %q", cityExpected, addr.DisplayName)
+		}
+		addr, err = coder.Reverse(t.Context(), cityLat, cityLon)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !addr.CacheHit {
+			t.Error("expected cache hit")
+		}
+	})
+	t.Run("reverse geocoding with town set should return the correct city", func(t *testing.T) {
+		rtFn := func(req *stdhttp.Request) (*stdhttp.Response, error) {
+			data, err := os.Open(townFile)
+			if err != nil {
+				t.Fatalf("failed to open JSON response file: %s", err)
+			}
+
+			return &stdhttp.Response{
+				StatusCode: 200,
+				Body:       data,
+				Header:     make(stdhttp.Header),
+			}, nil
+		}
+
+		coder := testCoderWithRoundtripFunc(t, rtFn)
+		addr, err := coder.Reverse(t.Context(), townLat, townLon)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !addr.AddressFound {
+			t.Fatal("expected address to be found")
+		}
+		if !strings.EqualFold(addr.City, townExpected) {
+			t.Errorf("expected city to be %q, got %q", townExpected, addr.DisplayName)
+		}
+	})
+	t.Run("reverse geocoding with village set should return the correct city", func(t *testing.T) {
+		rtFn := func(req *stdhttp.Request) (*stdhttp.Response, error) {
+			data, err := os.Open(villageFile)
+			if err != nil {
+				t.Fatalf("failed to open JSON response file: %s", err)
+			}
+
+			return &stdhttp.Response{
+				StatusCode: 200,
+				Body:       data,
+				Header:     make(stdhttp.Header),
+			}, nil
+		}
+
+		coder := testCoderWithRoundtripFunc(t, rtFn)
+		addr, err := coder.Reverse(t.Context(), villageLat, villageLon)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !addr.AddressFound {
+			t.Fatal("expected address to be found")
+		}
+		if !strings.EqualFold(addr.City, villageExpected) {
+			t.Errorf("expected city to be %q, got %q", villageExpected, addr.DisplayName)
+		}
+	})
+	t.Run("reverse geocoding fails", func(t *testing.T) {
+		rtFn := func(req *stdhttp.Request) (*stdhttp.Response, error) {
+			return nil, errors.New("intentionally failing")
+		}
+
+		coder := testCoderWithRoundtripFunc(t, rtFn)
+		_, err := coder.Reverse(t.Context(), cityLat, cityLon)
+		if err == nil {
+			t.Fatal("expected API request to fail")
+		}
+	})
+	t.Run("API responding with more than one result should fail", func(t *testing.T) {
+		response := Response{Features: []Feature{}}
+		rtFn := func(req *stdhttp.Request) (*stdhttp.Response, error) {
+			buf := bytes.NewBuffer(nil)
+			if err := json.NewEncoder(buf).Encode(response); err != nil {
+				return nil, err
+			}
+			return &stdhttp.Response{
+				StatusCode: 200,
+				Body:       io.NopCloser(buf),
+				Header:     make(stdhttp.Header),
+			}, nil
+		}
+		coder := testCoderWithRoundtripFunc(t, rtFn)
+		if coder == nil {
+			t.Fatal("expected a non-nil geocoder")
+		}
+		_, err := coder.Reverse(t.Context(), cityLat, cityLon)
+		if err == nil {
+			t.Fatal("expected error, got nil")
+		}
+		wantErr := "no address found for coordinates"
+		if !strings.EqualFold(err.Error(), wantErr) {
+			t.Errorf("expected error to be %q, got %q", wantErr, err)
+		}
+	})
+	t.Run("API responding with a non-200 reponse", func(t *testing.T) {
+		response := Response{Features: []Feature{{Properties: Properties{City: "Berlin"}}}}
+		rtFn := func(req *stdhttp.Request) (*stdhttp.Response, error) {
+			buf := bytes.NewBuffer(nil)
+			if err := json.NewEncoder(buf).Encode(response); err != nil {
+				return nil, err
+			}
+			return &stdhttp.Response{
+				StatusCode: 401,
+				Body:       io.NopCloser(buf),
+				Header:     make(stdhttp.Header),
+			}, nil
+		}
+		coder := testCoderWithRoundtripFunc(t, rtFn)
+		if coder == nil {
+			t.Fatal("expected a non-nil geocoder")
+		}
+		_, err := coder.Reverse(t.Context(), cityLat, cityLon)
+		if err == nil {
+			t.Fatal("expected error, got nil")
+		}
+		wantErr := "received non-positive response code from geocode.earth API: 401"
+		if !strings.EqualFold(err.Error(), wantErr) {
+			t.Errorf("expected error to be %q, got %q", wantErr, err)
+		}
+	})
+}
+
+func TestOpenCage_Reverse_integration(t *testing.T) {
+	testhelper.PerformIntegrationTests(t)
+	t.Run("reverse geocoding succeeds", func(t *testing.T) {
+		coder := testCoder(t)
+		addr, err := coder.Reverse(t.Context(), cityLat, cityLon)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !addr.AddressFound {
+			t.Fatal("expected address to be found")
+		}
+		if !strings.EqualFold(addr.DisplayName, cityExpected) {
+			t.Errorf("expected address to be %q, got %q", cityExpected, addr.DisplayName)
+		}
+	})
+}
+
+func testCoder(t *testing.T) geocode.Geocoder {
+	testHttpClient := http.New(logger.New(slog.LevelDebug))
+	testLang := language.English
+	apikey := os.Getenv("GEOCODEEARTH_APIKEY")
+	if apikey == "" {
+		t.Skip("no geocode.earth API key set, skipping tests")
+	}
+	return New(testHttpClient, testLang, apikey)
+}
+
+func testCoderWithRoundtripFunc(t *testing.T, fn func(req *stdhttp.Request) (*stdhttp.Response, error)) geocode.Geocoder {
+	testHttpClient := http.New(logger.New(slog.LevelDebug))
+	testHttpClient.Transport = testhelper.MockRoundTripper{Fn: fn}
+	testLang := language.English
+	apikey := os.Getenv("GEOCODEEARTH_APIKEY")
+	if apikey == "" {
+		t.Skip("no geocode.earth API key set, skipping tests")
+	}
+	return New(testHttpClient, testLang, apikey)
+}
