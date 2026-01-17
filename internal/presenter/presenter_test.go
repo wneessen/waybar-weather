3513906e005208201dce71d66a8c0b24453acac2
Winni Neessen <wn@neessen.dev>
2026-01-17 14:10:25 +0000

    Added unit test for presenter constructor
    - Introduced `TestNew` to verify successful creation of a new presenter instance.
    - Established groundwork for testing core functionality in the presenter package.
    
    Signed-off-by: Winni Neessen <wn@neessen.dev>

---
+++ internal/presenter/presenter_test.go
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: Winni Neessen <wn@neessen.dev>
+//
+// SPDX-License-Identifier: MIT
+
+package presenter
+
+import "testing"
+
+func TestNew(t *testing.T) {
+	t.Run("creating a new presenter succeeds", func(t *testing.T) {
+	})
+}
