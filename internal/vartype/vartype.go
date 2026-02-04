c516ebfc62b8de53d9af9987c918e493c3117f18
Winni Neessen <wn@neessen.dev>
2026-02-04 16:53:41 +0100

    Expanded `Variable` API functionality and documentation
    - Added comprehensive documentation for `Variable` struct and its methods (`Set`, `Reset`, `IsSet`, `Value`, `String`).
    - Enhanced `String` method with a more descriptive placeholder message for uninitialized variables.

--- internal/vartype/vartype.go
+++ internal/vartype/vartype.go
@@ -12,11 +12,13 @@ type VarFloat64 = Variable[float64]
 type VarInt = Variable[int]
 type VarBool = Variable[bool]
 
+// Variable represents a generic type wrapper that holds a value and tracks its initialization state.
 type Variable[T any] struct {
 	value T
 	isset bool
 }
 
+// NewVariable creates and returns a new Variable instance initialized with the provided value.
 func NewVariable[T any](value T) Variable[T] {
 	return Variable[T]{
 		isset: true,
@@ -24,28 +26,33 @@ func NewVariable[T any](value T) Variable[T] {
 	}
 }
 
+// Reset clears the value of the Variable and marks it as uninitialized.
 func (v *Variable[T]) Reset() {
 	var newVal T
 	v.value = newVal
 	v.isset = false
 }
 
+// Value retrieves the current value stored in the Variable.
 func (v *Variable[T]) Value() T {
 	return v.value
 }
 
+// Set assigns the provided value to the Variable and marks it as initialized.
 func (v *Variable[T]) Set(val T) {
 	v.value = val
 	v.isset = true
 }
 
+// IsSet returns true if the Variable has been initialized with a value, otherwise false.
 func (v *Variable[T]) IsSet() bool {
 	return v.isset
 }
 
+// String returns a string representation of the Variable. If uninitialized, it returns a default placeholder message.
 func (v Variable[T]) String() string {
 	if !v.isset {
-		return "Unsupported"
+		return "Unsupported by weather provider"
 	}
 	return fmt.Sprint(v.value)
 }
